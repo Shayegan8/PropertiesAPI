a095f332a9e9de1f0af8edf78f1ee29d6f60770e
shayegan8 <shayeganrood85@gmail.com>
2023-10-20 13:58:49 +0330

    Irrational things removed

--- PropertiesAPI.java
+++ PropertiesAPI.java
@@ -1092,3 +1092,57 @@ public class PropertiesAPI {
     }
 
 }
+
+class IsNumber {
+
+    private static List<Character> strchars;
+
+    static {
+        try {
+            strchars = new ArrayList<Character>();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean isNumber(String str) {
+        int i = 0;
+        char cstr[] = str.toCharArray();
+        while (i < cstr.length) {
+            strchars.add(cstr[i]);
+            i++;
+        }
+        i = 0;
+        while (i < strchars.size()) {
+            if (strchars.stream().filter(Character::isDigit).collect(Collectors.toList()).size() == strchars.size()) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
+    public static boolean isNumber(char str[]) {
+        int i = 0;
+        while (i < str.length) {
+            strchars.add(str[i]);
+            i++;
+        }
+        i = 0;
+        while (i < strchars.size()) {
+            if (strchars.stream().filter(Character::isDigit).collect(Collectors.toList()).size() == strchars.size()) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
+    public static boolean isNumber(char chr) {
+        return Character.isDigit(chr) ? true : false;
+    }
+
+    public static void fakeFree() {
+        strchars = null;
+    }
+}
